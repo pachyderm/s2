d516331e0c00179e7069114be1843f2a102e4962
Yusuf Simonson <simonson@gmail.com>
2020-05-15 10:21:36 -0400

    Wait for server to come up

--- etc/travis_test.sh
+++ etc/travis_test.sh
@@ -6,5 +6,11 @@ make ./conformance/s3-tests
 
 pushd examples/sql
     make run &
+
+    until (curl --connect-timeout 1 -s -D - http://localhost:8080 -o /dev/null | head -n1 | grep 403); do
+        echo -n '.'
+        sleep 1
+    done
+
     make test
 popd
