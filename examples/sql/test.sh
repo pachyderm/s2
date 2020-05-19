831871998e74267877698e2f2209c71d70abb957
Yusuf Simonson <simonson@gmail.com>
2020-05-19 10:44:08 -0400

    Removed old test runner

--- examples/sql/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-pushd ../../integration
-    . venv/bin/activate
-    S3_ADDRESS=http://localhost:8080 S3_ACCESS_KEY=homer S3_SECRET_KEY=donuts pytest test.py -k test_minio_bin
-popd
