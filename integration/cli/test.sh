a8458677926529e680e885f5699b7cbd8c475bc3
Yusuf Simonson <simonson@gmail.com>
2020-05-20 11:49:55 -0400

    Handle spaces in variables

--- integration/cli/test.sh
+++ integration/cli/test.sh
@@ -15,25 +15,25 @@ command -v aws >/dev/null 2>&1 || {
 set -ex
 
 if [ "$test_aws" = true ] ; then
-    export AWS_ACCESS_KEY_ID=$S2_ACCESS_KEY
-    export AWS_SECRET_ACCESS_KEY=$S2_SECRET_KEY
+    export AWS_ACCESS_KEY_ID="${S2_ACCESS_KEY}"
+    export AWS_SECRET_ACCESS_KEY="${S2_SECRET_KEY}"
 
-    aws s3 mb s3://test-aws-bin --endpoint=$S2_HOST_ADDRESS
-    aws s3 cp ../testdata/small.txt s3://test-aws-bin/small.txt --endpoint=$S2_HOST_ADDRESS
-    aws s3 cp ../testdata/large.txt s3://test-aws-bin/large.txt --endpoint=$S2_HOST_ADDRESS
-    aws s3 ls s3://test-aws-bin --endpoint=$S2_HOST_ADDRESS
+    aws s3 mb s3://test-aws-bin --endpoint="${S2_HOST_ADDRESS}"
+    aws s3 cp ../testdata/small.txt s3://test-aws-bin/small.txt --endpoint="${S2_HOST_ADDRESS}"
+    aws s3 cp ../testdata/large.txt s3://test-aws-bin/large.txt --endpoint="${S2_HOST_ADDRESS}"
+    aws s3 ls s3://test-aws-bin --endpoint="${S2_HOST_ADDRESS}"
 
     small_output=$(mktemp)
-    aws s3 cp s3://test-aws-bin/small.txt $small_output --endpoint=$S2_HOST_ADDRESS
-    cmp --silent ../testdata/small.txt "$small_output"
+    aws s3 cp s3://test-aws-bin/small.txt "${small_output}" --endpoint="${S2_HOST_ADDRESS}"
+    cmp --silent ../testdata/small.txt "${small_output}"
 
     large_output=$(mktemp)
-    aws s3 cp s3://test-aws-bin/large.txt $large_output --endpoint=$S2_HOST_ADDRESS
-    cmp --silent ../testdata/large.txt "$large_output"
+    aws s3 cp s3://test-aws-bin/large.txt "${large_output}" --endpoint="${S2_HOST_ADDRESS}"
+    cmp --silent ../testdata/large.txt "${large_output}"
 
-    aws s3 rm s3://test-aws-bin/small.txt --endpoint=$S2_HOST_ADDRESS
-    aws s3 rm s3://test-aws-bin/large.txt --endpoint=$S2_HOST_ADDRESS
-    aws s3 rb s3://test-aws-bin --endpoint=$S2_HOST_ADDRESS
+    aws s3 rm s3://test-aws-bin/small.txt --endpoint="${S2_HOST_ADDRESS}"
+    aws s3 rm s3://test-aws-bin/large.txt --endpoint="${S2_HOST_ADDRESS}"
+    aws s3 rb s3://test-aws-bin --endpoint="${S2_HOST_ADDRESS}"
 fi
 
 if [ "$test_minio" = true ] ; then
@@ -44,12 +44,12 @@ if [ "$test_minio" = true ] ; then
     mc ls s2/test-minio-bin
 
     small_output=$(mktemp)
-    mc cp s2/test-minio-bin/small.txt $small_output
-    cmp --silent ../testdata/small.txt "$small_output"
+    mc cp s2/test-minio-bin/small.txt "${small_output}"
+    cmp --silent ../testdata/small.txt "${small_output}"
 
     large_output=$(mktemp)
-    mc cp s2/test-minio-bin/large.txt $large_output
-    cmp --silent ../testdata/large.txt "$large_output"
+    mc cp s2/test-minio-bin/large.txt "${large_output}"
+    cmp --silent ../testdata/large.txt "${large_output}"
 
     mc rm s2/test-minio-bin/small.txt
     mc rm s2/test-minio-bin/large.txt
